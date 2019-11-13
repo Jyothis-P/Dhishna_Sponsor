01e6b178e97fb256690ac3763d135674524c79d4
Jyothis P <jyothisp52@gmail.com>
2019-11-13 16:38:35 +0530

    Displays data of individual

--- app/src/main/java/com/jyothisp/dhishnasponsor/ListActivity.java
+++ app/src/main/java/com/jyothisp/dhishnasponsor/ListActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +20,10 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -42,7 +47,6 @@ public class ListActivity extends AppCompatActivity {
         FirestoreRecyclerOptions<Company> options = new FirestoreRecyclerOptions.Builder<Company>()
                 .setQuery(query, Company.class)
                 .build();
-
 
 
         adapter = new FirestoreRecyclerAdapter<Company, CompanyViewHolder>(options) {
@@ -72,40 +76,68 @@ public class ListActivity extends AppCompatActivity {
         }
 
         void setProductName(Company company) {
+
+            final LinearLayout container = view.findViewById(R.id.item_container);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    container.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                }
+            });
             TextView textViewName = view.findViewById(R.id.cname_text_view);
             textViewName.setText(company.getName());
             TextView textViewLocale = view.findViewById(R.id.locale_text_view);
             textViewLocale.setText(company.getLocality());
             TextView textViewStatus = view.findViewById(R.id.status_text_view);
             textViewStatus.setText(company.getStatus());
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    editDoc();
-                }
-            });
-            try {
-                final String phone = company.getPhone();
-                view.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View v) {
-                        String posted_by = phone;
+            final String phone = company.getPhone();
 
-                        String uri = "tel:" + posted_by.trim() ;
+            SimpleDateFormat formatter = new SimpleDateFormat("dd MMM HH:mm");
+            Date date = new Date(company.getTime());
+            String time = formatter.format(date);
+
+            TextView textViewTime = view.findViewById(R.id.time_text_view);
+            textViewTime.setText(time);
+
+            view.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if (phone != null) {
+                        String posted_by = phone;
+                        String uri = "tel:" + posted_by.trim();
                         Intent intent = new Intent(Intent.ACTION_DIAL);
                         intent.setData(Uri.parse(uri));
                         startActivity(intent);
                         return true;
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Company contact unavailable", Toast.LENGTH_SHORT).show();
+                        return false;
                     }
-                });
-            } catch ( NullPointerException e){
-                Toast.makeText(getApplicationContext(), "Company contact unavailable", Toast.LENGTH_SHORT).show();
-            }
+                }
+            });
+
         }
     }
+
 
     private void editDoc() {
         Toast.makeText(getApplicationContext(), "edit", Toast.LENGTH_SHORT).show();
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (adapter != null) {
+            adapter.stopListening();
+        }
+    }
 }
