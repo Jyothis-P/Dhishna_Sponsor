f9dee6ae4a4a7435b5a5d95158127af86449e905
Jyothis P <jyothisp52@gmail.com>
2019-11-13 13:33:32 +0530

    Add data done

--- app/src/main/java/com/jyothisp/dhishnasponsor/MainActivity.java
+++ app/src/main/java/com/jyothisp/dhishnasponsor/MainActivity.java
@@ -4,33 +4,49 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 1;
     private static final String TAG = "MainActivity";
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    private EditText mNameEditText, mLocalityEditText, mStatus;
+    private Button mSubmitCompanyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
         mAuth = FirebaseAuth.getInstance();
-
-
+        db = FirebaseFirestore.getInstance();
         mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -40,9 +56,67 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mSubmitCompanyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addCompany();
+            }
+        });
+
 
     }
 
+    private void addCompany() {
+        String name = mNameEditText.getText().toString().trim();
+        String locality = mLocalityEditText.getText().toString().trim();
+        String status = mStatus.getText().toString().trim();
+
+        refreshViews();
+
+        String uid = mAuth.getUid();
+        String user = mAuth.getCurrentUser().getDisplayName();
+        Date currentTime = Calendar.getInstance().getTime();
+        String time = currentTime.toString();
+
+
+        Map<String, Object> company = new HashMap<>();
+        company.put("name", name);
+        company.put("locality", locality);
+        company.put("status", status);
+        company.put("user", user);
+        company.put("uid", uid);
+        company.put("time", time);
+
+        db.collection("companies")
+                .add(company)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Toast.makeText(getApplicationContext(), "Company remark submitted", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void initView(){
+        mNameEditText = findViewById(R.id.company_name);
+        mLocalityEditText = findViewById(R.id.company_locality);
+        mStatus = findViewById(R.id.company_status);
+        mSubmitCompanyButton = findViewById(R.id.submit_company);
+    }
+
+    private void refreshViews(){
+        mNameEditText.setText("");
+        mLocalityEditText.setText("");
+        mStatus.setText("");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,6 +133,10 @@ public class MainActivity extends AppCompatActivity {
         }
         else if (item.getItemId() == R.id.signout) {
             mAuth.signOut();
+            return true;
+        }
+        else if (item.getItemId() == R.id.getlist) {
+            startActivity(new Intent(MainActivity.this, ListActivity.class));
             return true;
         }
 
